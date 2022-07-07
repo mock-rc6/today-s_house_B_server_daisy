e5e646902aa5d7a7c36f267415a25767c7b187e5
LeeSoMyoung <so4644009@gmail.com>
2022-07-07 16:30:19 +0900

    fix: 캐시 삭제 후 .gitignore 내용 반영

--- src/main/java/com/example/demo/src/review/ReviewProvider.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.src.review;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class ReviewProvider {
-}
--- src/main/java/com/example/demo/src/review/dao/ReviewDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.src.review.dao;
-
-import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Repository;
-
-@Repository
-@AllArgsConstructor
-public class ReviewDao {
-}
